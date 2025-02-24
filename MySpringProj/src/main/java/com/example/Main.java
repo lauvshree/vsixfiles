525f12f78fcf66daa76663d0379eed01dca6207b
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2025-02-24 20:54:31 +1100

    Update Main.java

--- MySpringProj/src/main/java/com/example/Main.java
+++ MySpringProj/src/main/java/com/example/Main.java
@@ -1,15 +1,19 @@
 package com.example;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
 public class Main {
+    public static void main(String[] args) {
+        // Load XML-based configuration
+        ApplicationContext xmlContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloWorld helloWorldXml = (HelloWorld) xmlContext.getBean("helloWorld");
+        helloWorldXml.printMessage(); // Output: Message: Hello, World from XML!
 
-    @Bean
-    public HelloWorld helloWorld() {
-        HelloWorld helloWorld = new HelloWorld();
-        helloWorld.setMessage("Hello, World from Java Config!");
-        return helloWorld;
+        // Load Java-based configuration
+        ApplicationContext javaContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorldJava = javaContext.getBean(HelloWorld.class);
+        helloWorldJava.printMessage(); // Output: Message: Hello, World from Java Config!
     }
 }
