270c5f8732daf900eb5f358e1c3c8ba243678fcb
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2025-02-25 19:52:00 +1100

    Update Main.java

--- MySpringProj/src/main/java/com/example/Main.java
+++ MySpringProj/src/main/java/com/example/Main.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
